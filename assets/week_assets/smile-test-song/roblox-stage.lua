e3e843fd71bca73936a4ec70bdbfa90e67211e85
IceyBro <nicholaserharris2005@gmail.com>
2025-03-26 06:46:49 +0000

    ertewrt
    wswww

---
+++ assets/week_assets/smile-test-song/roblox-stage.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	makeLuaSprite('roblox-stage', 'smile-test-song', −700, −150); 
+	setScrollFactor('roblox-stage', 1, 1); 
+	scaleObject('roblox-stage', 1, 1); 
+
+	addLuaSprite('roblox-stage', false); 
+	close(true);
+end
